7ba1aa350b64b7119c4165ca3e91ac6269f9bb5a
Dmariedwards <Dmariedwards@gmail.com>
2018-04-22 12:48:55 -0400

    Cache control for city

--- server/api/city/city.controller.js
+++ server/api/city/city.controller.js
@@ -5,6 +5,7 @@ const Restaurant = require('../restaurant/restaurant.model')
 // GET /cities
 module.exports.list = (req, res, next) => {
     return Restaurant.find().distinct('address.city', (err, response) => {
+        res.setHeader('Cache-Control', 'max-age=604800, public');
         return res.status(200).send(response).end();
     }).catch(next);
 };
--- server/api/violation/violation.controller.js
+++ server/api/violation/violation.controller.js
@@ -5,7 +5,7 @@ const Violation = require('./violation.model')
 // GET /violations
 module.exports.list = (req, res, next) => {
     return Violation.find({}, '-_id').then((response) => {
-        res.setHeader('Cache-Control', 'max-age=604800, public')
+        res.setHeader('Cache-Control', 'max-age=604800, public');
         return res.status(200).send(response).end();
 
     }).catch(next);
