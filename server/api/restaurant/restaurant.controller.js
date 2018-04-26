dd0af911b29c77b36a217dab5a5fbb747979febf
Alexander Schwartzberg <aeksco@gmail.com>
2018-04-26 00:02:04 -0400

    Restaurant controller fixes

--- server/api/restaurant/restaurant.controller.js
+++ server/api/restaurant/restaurant.controller.js
@@ -1,52 +1,17 @@
 const Restaurant = require('./restaurant.model')
 
+// Defines the attributes returned when searching
+const searchResultAttrs = ['_id', 'facility', 'operation_name', 'corp_name', 'address.city', 'address.zipcode']
+
 // // // //
 
-
-// Query / Sort / Paginate
-const buildQuery = (opts) => {
-
-    // Defines the attributes returned when searching
-    const searchResultAttrs = ['_id', 'facility', 'operation_name', 'corp_name', 'address.city', 'address.zipcode']
-
-    // Defines return payload object
-    let payload = {};
-
-    // Handles default query
-    let query = opts.query || {};
-
-    // Defines Mongoose query
-    query = opts.schema.find(query, searchResultAttrs);
-
-    // Sort
-    if (opts.sort) {
-        query.sort(opts.sort);
-    }
-
-    // Pagination
-    if (opts.paginate) {
-
-        // Default pagination options
-        let page = Number(opts.page) || 0;
-        let per_page = Number(opts.per_page) || 20;
-        let skip = per_page * page;
-
-        // Applies pagination options to query
-        query.limit(per_page);
-        query.skip(skip);
-
-        // Assigns pagination parameters to payload
-        payload.page = page;
-        payload.per_page = per_page;
-
-    }
-
-    // Assigns query to payload
-    payload.query = query;
-
-    // Returns payload
-    return payload;
-
+// Defines default pagination options
+function handlePagination (req) {
+    let page = Number(req.query.page) || 1;
+    page = Math.max(page - 1, 0)
+    let per_page = Number(req.query.per_page) || 10;
+    let skip = per_page * page;
+    return { page, per_page, skip }
 }
 
 // // // //
@@ -54,26 +19,29 @@ const buildQuery = (opts) => {
 // GET /restaurants
 module.exports.list = (req, res, next) => {
 
-    // Build paginated query
-    let payload = buildQuery({
-        schema:     Restaurant,
-        paginate:   true,
-        page:       req.query.page,
-        per_page:   req.query.per_page
-    });
+    let query = {}
+    let { page, per_page, skip } = handlePagination(req)
 
-    // Returns paginated query
-    return payload.query.lean().exec().then( (response) => {
-
-        res.setHeader('Cache-Control', 'max-age=604800, public');
-
-        return res.status(200).send({
-            page:       payload.page,
-            per_page:   payload.per_page,
-            items:      response })
+    // Queries and Paginates
+    return Restaurant.find(query, searchResultAttrs)
+      .sort({ operation_name: 1 })
+      .limit(per_page)
+      .skip(skip)
+      .lean()
+      .exec()
+      .then((response) => {
+        return res
+        .status(200)
+        .json({
+          page: page + 1,
+          per_page: per_page,
+          items: response
+        })
         .end();
-
-    }).catch(next);
+      })
+      .catch((err) => {
+        handleError(res, err)
+      })
 };
 
 // // // //
@@ -81,48 +49,62 @@ module.exports.list = (req, res, next) => {
 // GET /restaurants/search
 module.exports.search = (req, res, next) => {
 
+    // Handles user queries
     let query = {}
+    let { page, per_page, skip } = handlePagination(req)
+    let searchQuery = req.query.q
 
+    // Regex matching for text search
     let matchQuery = [
-        { facility: new RegExp(req.query.q, 'i') },
-        { operation_name: new RegExp(req.query.q, 'i') },
-        { corp_name: new RegExp(req.query.q, 'i') }
+        { facility: new RegExp(searchQuery, 'i') },
+        { operation_name: new RegExp(searchQuery, 'i') },
+        { corp_name: new RegExp(searchQuery, 'i') }
     ]
 
+    // Builds query
     if (req.query.city) {
         query['$and'] = [
             { '$or': matchQuery },
             { 'address.city': req.query.city }
         ]
     } else {
-        query = matchQuery
+        query['$and'] = [
+            { '$or': matchQuery }
+        ]
     }
 
-    // Build paginated query
-    let payload = buildQuery({
-        schema:     Restaurant,
-        paginate:   true,
-        page:       req.query.page,
-        per_page:   req.query.per_page,
-        query:      query
-    });
-
-    // Returns paginated query
-    return payload.query.lean().exec().then( (response) => {
-        return res.status(200).send({
-            page:       payload.page,
-            per_page:   payload.per_page,
-            items:      response })
+    // Queries and Paginates
+    return Restaurant.find(query, searchResultAttrs)
+      .sort({ operation_name: 1 })
+      .limit(per_page)
+      .skip(skip)
+      .lean()
+      .exec()
+      .then((response) => {
+        return res
+        .status(200)
+        .json({
+          page: page + 1,
+          per_page: per_page,
+          items: response
+        })
         .end();
-    }).catch(next);
+      })
+      .catch((err) => {
+        handleError(res, err)
+      })
 };
 
 // // // //
 
 // GET /restaurants/:id
 module.exports.show = (req, res, next) => {
-    return Restaurant.findOne({ _id: req.params.id }).then((response) => {
-        return res.status(200).send(response).end();
+    return Restaurant.findOne({ _id: req.params.id })
+    .then((response) => {
+        return res
+        .status(200)
+        .send(response)
+        .end();
     }).catch(next);
 };
 
